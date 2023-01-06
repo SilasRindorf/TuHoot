a2ba7adc6639aa447c09ec42a6ea8b14ab03c99d
SilasRindorf <s195474@student.dtu.dk>
2023-01-06 12:01:57 +0100

    Updated .gitignore

--- src/main/java/dtu/group8/client/Client.java
+++ src/main/java/dtu/group8/client/Client.java
@@ -1,5 +1,7 @@
 package dtu.group8.client;
 
+import org.jspace.ActualField;
+import org.jspace.FormalField;
 import org.jspace.RemoteSpace;
 
 import java.io.BufferedReader;
@@ -21,7 +23,9 @@ import java.net.UnknownHostException;
  *      Reconnect to game
  */
 public class Client {
-
+    private final String port = "9001";
+    private final String serverIP = "127.0.0.1:9001";
+    private String temp = "10.209.95.114:" + port;
     public void start() {
         try {
 
@@ -33,7 +37,7 @@ public class Client {
             String uri = input.readLine();
             // Default value
             if (uri.isEmpty()) {
-                uri = "tcp://127.0.0.1:9001/chat?keep";
+                uri = "tcp://"+ temp + "/chat?keep";
             }
 
             // Connect to the remote chat space
@@ -44,8 +48,15 @@ public class Client {
             System.out.print("Enter your name: ");
             String name = input.readLine();
 
+            // Read game id from the console
+            System.out.print("Enter game ID: ");
+            String gameID = input.readLine();
+            String clientID = String.valueOf(Math.random());
+            chat.put("add",gameID,name,clientID);
+            chat.get(new ActualField(clientID),new FormalField(String.class));
+            System.out.println("Type start to begin");
+
             // Keep sending whatever the user types
-            System.out.println("Start chatting...");
             while (true) {
                 String message = input.readLine();
                 chat.put(name, message);
