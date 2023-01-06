c92a3a6ff872481c0feca26999c7870a77117108
SilasRindorf <s195474@student.dtu.dk>
2023-01-06 09:45:32 +0100

    Added jSpace dependency

--- src/main/java/dtu/group8/Main.java
+++ src/main/java/dtu/group8/Main.java
@@ -1,4 +1,10 @@
-package dtu.group8;public class Main {
+package dtu.group8;
+
+import org.jspace.ActualField;
+import org.jspace.SequentialSpace;
+import org.jspace.Space;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
