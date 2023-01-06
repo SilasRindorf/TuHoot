c971f898b3a1e84f688b114e60a195fe9454a4db
SilasRindorf <s195474@student.dtu.dk>
2023-01-06 10:05:39 +0100

    Added server package, added client package

--- src/main/java/dtu/group8/Main.java
+++ src/main/java/dtu/group8/Main.java
@@ -5,6 +5,7 @@ import org.jspace.SequentialSpace;
 import org.jspace.Space;
 
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
---
+++ src/main/java/dtu/group8/client/Client.java
@@ -0,0 +1,17 @@
+package dtu.group8.client;
+
+/**
+ * Client
+ * Responsibilities:
+ * Must have:
+ *      Receive questions
+ *      Receive answer options
+ *      Send answers
+ * Can have:
+ *      See opponent points
+ *      See timer
+ *      See amount of remaining questions
+ *      Reconnect to game
+ */
+public class Client {
+}
