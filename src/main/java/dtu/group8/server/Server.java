a8e4cd0a3df34bb44aa42b0213a769eec63bd519
Zahed <zahedm45@gmail.com>
2023-01-09 10:06:52 +0100

    Added the test client

--- src/main/java/dtu/group8/server/Server.java
+++ src/main/java/dtu/group8/server/Server.java
@@ -8,8 +8,6 @@ import org.jspace.SpaceRepository;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.LinkedList;
 
 public class Server {
@@ -23,19 +21,14 @@ public class Server {
     public static void main(String[] args) {
 
         Server server = new Server();
-        server.start();
-    }
-    public Server(){
+        server.startServer();
     }
 
-    public void start() {
+    public void startServer() {
         try {
             BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-            // Create a repository
             SpaceRepository repository = new SpaceRepository();
-            // Create a local space for the chat messages
             SequentialSpace spaceLobby = new SequentialSpace();
-            // Add the space to the repository
             repository.add("lobby", spaceLobby);
             // Set the URI of the chat space
             System.out.print("Enter URI of the chat server or press enter for default: ");
@@ -61,7 +54,6 @@ public class Server {
                 String newSpaceId = "boardId" + spaceCounter;
                 repository.add(newSpaceId, newSpace);
                 newSpace.put("Hello");
-
 
                 for (Object[] client : obj) {
                     spaceLobby.put(client[2], newSpaceId);
