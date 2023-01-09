e43ffe5dc82e1fdf72a73bc5c6205b4786dac198
Zahed <zahedm45@gmail.com>
2023-01-09 19:59:46 +0100

    ThreadStartGame almost done

--- src/main/java/dtu/group8/client/ThreadStartGame.java
+++ src/main/java/dtu/group8/client/ThreadStartGame.java
@@ -1,6 +1,7 @@
 package dtu.group8.client;
 
 
+import dtu.group8.server.model.Player;
 import org.jspace.ActualField;
 import org.jspace.FormalField;
 import org.jspace.RemoteSpace;
@@ -9,9 +10,7 @@ import org.jspace.Space;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
+import java.util.*;
 
 public class ThreadStartGame implements Runnable {
     private final String OPTIONS = "Options:\n\t1. start game\n\tor wait to get an invitation";
@@ -19,12 +18,15 @@ public class ThreadStartGame implements Runnable {
     private boolean isAlive = true;
     private boolean amIHost = false;
     private static final String LOCK_FOR_GAME_START = "lockForGameStart";
+    private Player player;
+    //BufferedReader input;
 
 
 
 
-    public ThreadStartGame(Space space) {
+    public ThreadStartGame(Space space, Player player) {
         this.space = space;
+        this.player = player;
     }
 
     @Override
@@ -33,20 +35,26 @@ public class ThreadStartGame implements Runnable {
             while (isAlive) {
                 System.out.println(OPTIONS);
                 System.out.print("Input command: ");
-                BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+                //input = new BufferedReader(new InputStreamReader(System.in));
+                Scanner scanner = new Scanner(System.in);
 
-                String userInput = input.readLine();
+
+
+                String userInput = scanner.nextLine();
                 if (userInput.equalsIgnoreCase("1") || userInput.equalsIgnoreCase("start game")){
                     Object[] obj = space.getp(new ActualField(LOCK_FOR_GAME_START));
 
                     if (obj[0].equals(LOCK_FOR_GAME_START)) {
 
                         Object[] membersObj = space.get(new ActualField("allMembers"), new FormalField(Object.class), new FormalField(Object.class));
-
                         String[] pids = (String[]) membersObj[2];
 
+                        for (String pid : pids) {
+                            space.put(pid, "join me", player.getName());
+                        }
 
-                        space.put("joinMe");
+
+
                         amIHost = true;
                         space.put(LOCK_FOR_GAME_START);
                         break;
@@ -57,7 +65,7 @@ public class ThreadStartGame implements Runnable {
 
                 }
             }
-        } catch (IOException | InterruptedException e) {
+        } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
     }
@@ -65,31 +73,35 @@ public class ThreadStartGame implements Runnable {
     public void setAlive(boolean alive) {
         isAlive = alive;
     }
+
+    public boolean isAmIHost() {
+        return amIHost;
+    }
 }
 
+class ThreadAcknowledgement implements Runnable {
+    Space space;
+    String[] pids;
 
-class Player2 {
-    private String name;
-    private String id;
-    private int point;
-
-
-    Player2(String name, String id, int point) {
-        this.name = name;
-        this.id = id;
-        this.point = point;
+    public ThreadAcknowledgement(Space space, String[] pids) {
+        this.space = space;
+        this.pids = pids;
     }
 
+    @Override
+    public void run() {
+       HashMap<String, String> receivedPids = new HashMap<>();
 
-    public String getName() {
-        return name;
+        try {
+            for (int i = 0; i < pids.length; i++) {
+                Object[] obj = space.get(new ActualField("ack"), new FormalField(Object.class), new FormalField(Object.class));
+                receivedPids.put(obj[1].toString(), obj[2].toString());
+            }
+            System.out.println(receivedPids);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public int getPoint() {
-        return point;
-    }
 }
