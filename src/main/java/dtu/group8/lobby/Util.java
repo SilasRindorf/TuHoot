a8b05831c871c4137a35ed6bf586af675e490970
Zahed <zahedm45@gmail.com>
2023-01-12 14:57:20 +0100

    Clean up Util.java

--- src/main/java/dtu/group8/lobby/Util.java
+++ src/main/java/dtu/group8/lobby/Util.java
@@ -1,9 +1,5 @@
 package dtu.group8.lobby;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class Util {
     public final static String JOINT_REQ_FROM_SERVER = "join_req_from_server";
     public final static String JOINT_RES_FROM_HOST = "join_res_from_host";
@@ -13,8 +9,6 @@ public class Util {
     public final static String PATTERN_FOR_PLAYER_ID_SPLITTER = "::";
     public final static String GAME_START = "game-start";
 
-
-
     public final static String ALL_PLAYERS = "allPlayers";
     public final static String CREATE_GAME_REQ = "create game";
     public final static String MY_SPACE_ID = "mySpaceId";
@@ -23,16 +17,4 @@ public class Util {
     public static final String IP = "localhost";
     public static final String TYPE = "?keep";
 
-
-    public static String takeUserInput() {
-        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        String userInput;
-        try {
-            userInput = input.readLine();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        System.out.println("Game: " + userInput);
-        return userInput;
-    }
 }
